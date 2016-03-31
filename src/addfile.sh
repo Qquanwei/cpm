a33563d757e6f524b07249be4403ddf0379f3e8e
quanwei <quanwei9958@126.com>
2016-03-31 12:47:05 +0800

    修改addfile支持basedir

--- src/addfile.sh
+++ src/addfile.sh
@@ -4,16 +4,25 @@
 # 规则: 将文件添加到本地cinclude中
 
 function print_usag_and_exit(){
-  echo "err params:"
-  echo "       ${1} header.h"
+  echo "参数错误:"
+  echo "   ${1} header.h basedir"
 }
-if [ "$#" = "1" ];then
-  mkdir cinclude 2>/dev/null
+if [ "$#" -gt 0 ];then
   filename=$1
+  #设置工作路径
+  BASEDIR=$(pwd)
+  if [  ! -z "$2" ];then
+    BASEDIR=$2
+  fi
+
+  if [ ! -d "${BASEDIR}/cinclude" ];then
+    mkdir -p "${BASEDIR}/cinclude"
+  fi
+
   if [ "${filename:0:4}" = "http" ];then
-    wget -nv -c --progress=bar -T 1200 -P "$(pwd)/cinclude" $filename
+    wget -nv -c --progress=bar -T 1200 -P "${BASEDIR}/cinclude" $filename
   else
-    \cp -f $1 "cinclude/"
+    \cp -f $1 "${BASEDIR}/cinclude/"
   fi
 else 
   print_usag_and_exit $0
