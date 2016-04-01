7aa4fd6dadd41d96368557f6a0c84c211fe4d3a9
quanwei <quanwei9958@126.com>
2016-04-01 18:18:06 +0800

    cpm remove 改变变量名

--- src/cpm_remove.sh
+++ src/cpm_remove.sh
@@ -7,12 +7,13 @@ function print_and_exit(){
 INSTALL_PATH=$(pwd)
 if [ $# -gt 0 ];then
   cd ${INSTALL_PATH}/cinclude
-  if [ -d "$1" ];then
+  packname=$1
+  if [ -d "$packname" ];then
     echo '正在删除..'
-    rm -rf ${INSTALL_PATH}/cinclude/$1
+    rm -rf ${INSTALL_PATH}/cinclude/$packname
     echo '删除完成'
   else
-    echo '该程序库不存在'
+    echo "该程序库不存在:$packname"
   fi
 else
   print_and_exit $0
